b06a0cc3f634e2a163b57de3c86268f0e058f3a5
SpencerHedger <spencer.hedger@outlook.com>
2019-11-07 22:02:59 +0000

    Fix value ranging and generate default filter

--- js/jsonstat-d3-bar-chart.js
+++ js/jsonstat-d3-bar-chart.js
@@ -4,7 +4,9 @@ function JSONstatD3BarChart(options) {
     options.colors = options.colors || ["#ca0020","#f4a582","#d5d5d5","#92c5de","#0571b0"]
     if(options.xSpacing === undefined) options.xSpacing = 10;
     if(options.animate === undefined) options.animate = true;
+    if(options.dataset === undefined) options.dataset = 0;
 
+    var _ds = options.data.Dataset(options.dataset);
     var _tgt = options.target;
     var x0 = d3.scaleBand();
     var x1 = d3.scaleBand();
@@ -18,21 +20,63 @@ function JSONstatD3BarChart(options) {
     var svg_g_yaxis = svg_g.append("g")
         .attr("class", "y axis");
 
+    // Create a filter with valid default values
+    function createDefaultFilter() {
+        var filter = {};
+        _ds.Dimension().map(d => {
+            if((d.label != options.x) && (d.label != options.y)) filter[d.label] = d.id[0];
+        });
+        return filter;
+    }
+
+    if(options.filter === undefined || options.filter == null) options.filter = createDefaultFilter();
+
     function redraw() {
-        var _ds = options.data.Dataset(0);
+        _ds = options.data.Dataset(options.dataset);
+
+        if(options.filter === undefined || options.filter == null) options.filter = createDefaultFilter();
+
         var chartHeight = options.height - options.margin.top - options.margin.bottom;
         var xNames = _ds.Dimension(options.x).id;
         var yNames = _ds.Dimension(options.y).id;
         
+        // Get a list of all values
+        function allValues() {
+            var filter = options.filter;
+            var values = [];
+            var xDim = _ds.Dimension(options.x);
+
+            // Loop through x
+            for(var i = 0; i < xDim.length; i++) {
+                filter[options.x] = xDim.id[i];
+
+                var yDim = _ds.Dimension(options.y);
+
+                // Loop through y
+                for(var j = 0; j < yDim.length; j++) {
+                    filter[options.y] = yDim.id[j];
+                    values.push(_ds.Data(filter).value);
+                }
+            }
+
+            return values;
+        }
+
+        // Find the maximum data value
+        function maxValue() {
+            // Return maximum of values
+            return d3.max(allValues());
+        }
+
         // Retrieve a value from the data for a given x and y dimension value.
         function dsValue(x, y) {
-            var filter = {};
-            filter[options.x] = x;
-            filter[options.y] = y;
+            options.filter = options.filter || {};
+            options.filter[options.x] = x;
+            options.filter[options.y] = y;
 
-            var value = _ds.Data(filter).value;
+            var value = _ds.Data(options.filter).value;
 
-            console.log({ filter: filter, value: value });
+            console.log({ filter: options.filter, value: value });
             return value;
         }
 
@@ -56,7 +100,7 @@ function JSONstatD3BarChart(options) {
         x1.domain(yNames)
             .range([options.xSpacing, x0.bandwidth()-options.xSpacing]);
 
-        y.domain([0, d3.max(_ds.value)]);
+        y.domain([0, maxValue()]);
 
         svg.attr("width", options.width + options.margin.left + options.margin.right)
             .attr("height", chartHeight + options.margin.top + options.margin.bottom);
