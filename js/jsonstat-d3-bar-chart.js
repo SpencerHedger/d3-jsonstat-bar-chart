38a6a22de8cef5b8fe554bd4784128c9cce970ef
SpencerHedger <spencer.hedger@outlook.com>
2019-11-07 20:50:42 +0000

    Seperate element creation and updating

--- js/jsonstat-d3-bar-chart.js
+++ js/jsonstat-d3-bar-chart.js
@@ -5,11 +5,22 @@ function JSONstatD3BarChart(options) {
     if(options.xSpacing === undefined) options.xSpacing = 10;
     if(options.animate === undefined) options.animate = true;
 
-    var _ds = options.data.Dataset(0);
     var _tgt = options.target;
-    var chartHeight = options.height - options.margin.top - options.margin.bottom;
+    var x0 = d3.scaleBand();
+    var x1 = d3.scaleBand();
+    var y = d3.scaleLinear();
+    var xAxis = d3.axisBottom();
+    var yAxis = d3.axisLeft();
+    var svg = d3.select(options.target).append("svg");
+    var svg_g = svg.append("g");
+    var svg_g_xaxis = svg_g.append("g")
+        .attr("class", "x axis");
+    var svg_g_yaxis = svg_g.append("g")
+        .attr("class", "y axis");
 
-    function draw() {
+    function redraw() {
+        var _ds = options.data.Dataset(0);
+        var chartHeight = options.height - options.margin.top - options.margin.bottom;
         var xNames = _ds.Dimension(options.x).id;
         var yNames = _ds.Dimension(options.y).id;
         
@@ -25,113 +36,107 @@ function JSONstatD3BarChart(options) {
             return value;
         }
 
-        var x0 = d3.scaleBand()
-            .range([0, options.width - options.margin.left - options.margin.right])
+        svg_g.attr("transform", "translate(" + options.margin.left + "," + options.margin.top + ")");
+
+        x0.range([0, options.width - options.margin.left - options.margin.right])
             .round(.1);
 
-        var y = d3.scaleLinear()
-            .range([chartHeight, 0]);
+        y.range([chartHeight, 0]);
 
-        var xAxis = d3.axisBottom()
-            .scale(x0)
+        xAxis.scale(x0)
             .tickSize(5);
 
-        var yAxis = d3.axisLeft()
-            .scale(y);
+        yAxis.scale(y);
 
         var color = d3.scaleOrdinal()
             .range(options.colors);
 
         x0.domain(xNames);
         
-        var x1 = d3.scaleBand()
-            .domain(yNames)
+        x1.domain(yNames)
             .range([options.xSpacing, x0.bandwidth()-options.xSpacing]);
 
         y.domain([0, d3.max(_ds.value)]);
 
-        var svg = d3.select(options.target).append("svg")
-        .attr("width", options.width + options.margin.left + options.margin.right)
-        .attr("height", chartHeight + options.margin.top + options.margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + options.margin.left + "," + options.margin.top + ")");
+        svg.attr("width", options.width + options.margin.left + options.margin.right)
+            .attr("height", chartHeight + options.margin.top + options.margin.bottom);
 
-        svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + chartHeight + ")")
-        .call(xAxis);
+        svg_g_xaxis.attr("transform", "translate(0," + chartHeight + ")")
 
-        svg.append("g")
-        .attr("class", "y axis")
-        .style('opacity','1')
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .style('font-weight','bold')
-        .text("Value");
+        var t = d3.transition().duration(500);
+        svg_g_xaxis.call(xAxis);
+        svg_g_yaxis.call(yAxis);
 
-        var slice = svg.selectAll(".slice")
-        .data(_ds.Dimension(options.x).id)
-        .enter().append("g")
-        .attr("class", "g")
-        .attr("transform", d => "translate(" + x0(d) + ",0)");
+        svg_g.selectAll(".slice")
+            .data(_ds.Dimension(options.x).id)
+            .enter().append("g")
+                .attr("class", "slice");
+        
+        svg_g.selectAll('.slice')
+            .attr("transform", d => "translate(" + x0(d) + ",0)");
 
-        slice.selectAll("rect")
-        .data(function(x) {
-            return _ds.Dimension(options.y).id.map(function (y) {
-                return { x: x, y: y, value: dsValue(x, y) };
-            });
+        var rects = svg_g.selectAll(".slice").selectAll('rect')
+            .data(function(x) {
+                return _ds.Dimension(options.y).id.map(function (y) {
+                    return { x: x, y: y, value: dsValue(x, y) };
+                });
             })
             .enter().append("rect")
-            .attr("width", x1.bandwidth())
+                .attr("y", d => options.animate? y(0) : y(d.value))
+                .attr("height", d => options.animate? 0 : chartHeight - y(d.value));
+
+        var rects = svg_g.selectAll('.slice rect');
+
+        rects.attr("width", x1.bandwidth())
             .attr("x", d => x1(d.y))
-            .style("fill", d => color(d.y))
-            .attr("y", d => options.animate? y(0) : y(d.value))
-            .attr("height", d => options.animate? 0 : chartHeight - y(d.value));
+            .style("fill", d => color(d.y));
 
         if(options.animate) {
-            slice.selectAll("rect")
-                .transition()
-                .delay(500)
-                .duration(1000)
+            rects.transition(t)
                 .attr("y", d => y(d.value))
                 .attr("height", d => chartHeight - y(d.value));
         }
+        else {
+            rects.attr("y", d => y(d.value))
+                .attr("height", d => chartHeight - y(d.value));
+        }
 
-        slice.exit().remove();
+        rects.exit().remove();
         
         // Legend
-        var legend = svg.selectAll(".legend")
+        var legend_enter = svg_g.selectAll(".legend")
             .data(yNames)
-                .enter().append("g")
-                    .attr("class", "legend")
-                    .attr("transform", function(d,i) { return "translate(0," + (i * 20) + ")"; });
+                .enter()
+                .append("g")
+                    .attr("class", "legend");
 
-        legend.append("rect")
-            .attr("x", options.width - 18)
+        svg_g.selectAll('.legend').append("rect")
+            .attr('class', 'legend-key')
             .attr("width", 18)
             .attr("height", 18)
+
+        svg_g.selectAll('.legend').append("text")
+            .attr('class', 'legend-text')
+            .attr("dy", ".35em")
+            .attr("y", 9)
+            .style("text-anchor", "end");
+
+        svg_g.selectAll('.legend-key')
+            .attr("x", options.width - 18)
             .style("fill", d => color(d));
 
-        legend.append("text")
+        svg_g.selectAll('.legend-text')
             .attr("x", options.width - 24)
-            .attr("y", 9)
-            .attr("dy", ".35em")
-            .style("text-anchor", "end")
             .text(d => d);
-    }
-
-    function refresh() {
         
+        svg_g.selectAll('.legend')
+            .attr("transform", function(d,i) { return "translate(0," + (i * 20) + ")"; });
     }
 
-    draw();
+    redraw();
 
     return {
-        refresh: refresh,
+        refresh: redraw,
         options: options
     };
 }
